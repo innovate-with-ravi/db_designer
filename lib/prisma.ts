39e76271a127ade8b7c43a06ef0b18542564a6f3
innovare-with-ravi <raviggbhawsar9090@gmail.com>
2026-06-24 12:06:40 +0530

    switched to postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
     globalForPrisma.prisma ||
-    // @ts-ignore
-    new PrismaClient()
+    new PrismaClient({
+        adapter: new PrismaPg({
+            connectionString: process.env.DATABASE_URL as string,
+        }),
+        log: ["warn", "error"],
+    });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
