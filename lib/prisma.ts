438c68f2aa377f3bb9c3ebd5c5288fb100439059
innovare-with-ravi <raviggbhawsar9090@gmail.com>
2026-06-24 11:53:23 +0530

    .

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,10 @@
-// import "dotenv/config";
-// import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-// import { PrismaClient } from "@/generated/prisma/client";
-
-// const adapter = new PrismaMariaDb({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     connectionLimit: 5,
-//     allowPublicKeyRetrieval: true,
-// });
-// const prisma = new PrismaClient({ adapter });
-
-// export { prisma };
-
 import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
     globalForPrisma.prisma ||
+    // @ts-ignore
     new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
